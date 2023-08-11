28a5d1b06743725e9f1c20bbd3fc333121b1b9d6
marklidenberg <marklidenberg@gmail.com>
2023-08-11 15:21:24 +0600

    update

--- source/python/libs/lessmore.utils/lessmore/utils/round_datetime.py
+++ source/python/libs/lessmore.utils/lessmore/utils/round_datetime.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 
-from lessmore.utils.numeric.numeric import custom_round
+from lessmore.utils.round.relative_round import relative_round
 from lessmore.utils.to_anything.unified_datetime import to_datetime, to_timestamp
 from lessmore.utils.to_anything.unified_timedelta import to_seconds, to_timedelta
 
@@ -8,7 +8,7 @@ from lessmore.utils.to_anything.unified_timedelta import to_seconds, to_timedelt
 def round_datetime(dt_obj, td_obj, rounding="nearest_half_even"):
     assert to_timedelta(td_obj) < timedelta(days=1), "Only rounding less than a day is supported."
     ts = to_timestamp(dt_obj)
-    ts = custom_round(a=ts, b=to_seconds(td_obj), rounding=rounding)
+    ts = relative_round(a=ts, b=to_seconds(td_obj), rounding=rounding)
     return to_datetime(ts)
 
 
