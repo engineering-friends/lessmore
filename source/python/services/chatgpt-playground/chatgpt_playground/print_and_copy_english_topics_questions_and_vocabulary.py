0f0dad3506554cd3a71014253350dfc59efb1a1e
marklidenberg <marklidenberg@gmail.com>
2023-08-15 23:23:25 +0600

    feat: chatgpt playgrounds

---
+++ source/python/services/chatgpt-playground/chatgpt_playground/print_and_copy_english_topics_questions_and_vocabulary.py
@@ -0,0 +1,27 @@
+from chatgpt_playground.make_request import make_request
+from chatgpt_playground.prompt import PROMPT
+from chatgpt_playground.topics import TOPICS
+from tqdm import tqdm
+
+from lessmore.utils.easy_printing.print_and_copy import print_and_copy
+
+
+def print_and_copy_english_topics_questions_and_vocabulary() -> None:
+    # - Get topics info from openai
+
+    topic_to_text = {topic: make_request(PROMPT, topic=topic) for topic in tqdm(TOPICS)}
+
+    # - Return formatted text
+
+    NEW_LINE = "\n"
+    text = "\n".join(
+        [f"""- {topic}\n    {text.replace(NEW_LINE, NEW_LINE + "    ")}""" for topic, text in topic_to_text.items()]
+    )
+
+    # - Print and copy
+
+    print_and_copy(text)
+
+
+if __name__ == "__main__":
+    print_and_copy_english_topics_questions_and_vocabulary()
---
+++ source/python/services/chatgpt-playground/chatgpt_playground/prompt.py
@@ -0,0 +1,68 @@
+PROMPT = """ for the topic {topic} generate a list of common IELTS part 1 questions along with a common vocabulary.
+
+    Example for topic “work”:
+
+    # Questions
+
+    - What is your job?
+    - Where do you work?
+    - Why did you choose that job?
+    - Is it a popular job in your country?
+    - Do you like your job?
+    - Do you get on well with your colleagues?
+    - What was your first day like?
+    - What responsibilities do you have at work?
+    - If you had the chance, would you change your job?
+    - Do you plan to continue with your job in the future?
+    - Do you think job satisfaction is more important than salary when choosing a job?
+    - What skills do you think are needed to get a good job these days?
+    - Do you think women should be able to do all the same jobs that men do?
+
+    # Vocabulary
+
+    1. Job
+    2. Career
+    3. Employment
+    4. Profession
+    5. Salary, wage
+    6. Occupation
+    7. Workforce
+    8. Productivity
+    9. Labor
+    10. Task
+    11. Effort
+    12. Hours
+    13. Overtime
+    14. Skills
+    15. Experience
+    16. Duties
+    17. Performance
+    18. Promotion
+    19. Workplace and Office
+    20. Team
+    21. Manager, Coworker and subordinate (and superordinate)
+    22. Leadership
+    23. Training
+    24. Responsibility
+    25. Deadlines
+    26. Project
+    27. Goals
+    28. Work-life balance
+    29. Benefits
+    30. Employee
+    31. Contractor
+    32. Business
+    33. Entrepreneurship
+    34. Discipline
+    35. Work ethics
+    36. Commitment
+    37. Motivation
+    38. Recognition
+    39. Job security
+    40. Resume
+    41. Interview
+    42. Part-time
+    43. Full-time
+    44. Shift
+    45. Remote work
+        """
