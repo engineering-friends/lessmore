bfde375619f93aec1015275fc626f3577ab16153
marklidenberg <marklidenberg@gmail.com>
2023-08-16 00:45:30 +0600

    refactor: even better refactoring

---
+++ source/python/services/discord-to-telegram-forwarder/discord_to_telegram_forwarder/send_discord_post_to_telegram/format_telegram_message_text.py
@@ -0,0 +1,79 @@
+import random
+
+from discord_to_telegram_forwarder.send_discord_post_to_telegram.get_shortened_url_from_tiny_url import (
+    get_shortened_url_from_tiny_url,
+)
+from discord_to_telegram_forwarder.send_discord_post_to_telegram.request_emoji_from_openai import (
+    request_emoji_from_openai,
+)
+from inline_snapshot import snapshot
+
+from lessmore.utils.backlog.run_inline_snapshot_tests import run_inline_snapshot_tests
+from lessmore.utils.easy_printing.print_and_copy import print_and_copy
+from lessmore.utils.file_helpers.read_file import read_file
+from lessmore.utils.path_helpers.get_current_dir import get_current_dir
+
+
+def format_telegram_message_text(
+    post_author_name: str,
+    post_body: str,
+    post_forum_channel_name: str,
+    post_title: str,
+    post_url: str,
+    add_inner_shortened_url: bool,
+):
+    # - Read emoticons
+
+    emoticons = read_file(str(get_current_dir() / "emoticons.txt")).strip().split("\n")
+
+    # - Get emoji from openai
+
+    emoji = request_emoji_from_openai(f"{post_forum_channel_name} {post_title} {post_body}")
+
+    # - Make discord schema and shorten it to make it https:// with redirection to discord://
+
+    inner_shortened_url = (
+        get_shortened_url_from_tiny_url(post_url.replace("https", "discord")) if add_inner_shortened_url else ""
+    )
+
+    # - Format message for telegram
+
+    text = ""
+    if post_forum_channel_name:
+        text += f"#{post_forum_channel_name.replace('-', '_')}\n"
+    text += f"{emoji} **{post_title}**\n\n"
+    if post_body:
+        text += (
+            post_body[:3000] + ("" if len(post_body) < 3000 else "...") + "\n\n"
+        )  # maximum telegram message size is 4096. Making it 3000 to resever space for title and for the buffer
+    text += f"{post_author_name} {random.choice(emoticons)}\n"
+    text += f"[→ к посту]({post_url})"
+    if inner_shortened_url:
+        text += f"\n[→ к посту на apple-устройствах]({inner_shortened_url})"
+    return text
+
+
+def test():
+    random.seed(12)
+
+    assert (
+        format_telegram_message_text(
+            post_forum_channel_name="channel_name",
+            post_title="Тестирую форвардер",
+            post_body="",
+            post_author_name="Mark Lidenberg",
+            post_url="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley",
+            add_inner_shortened_url=True,
+        )
+    ) == snapshot(
+        """#channel_name
+🔍 **Тестирую форвардер**
+
+Mark Lidenberg (。◕‿‿◕。)
+[→ к посту](https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley)
+[→ к посту на apple-устройствах](https://tinyurl.com/2bub4s6g)"""
+    )
+
+
+if __name__ == "__main__":
+    run_inline_snapshot_tests()
--- source/python/services/discord-to-telegram-forwarder/discord_to_telegram_forwarder/send_discord_post_to_telegram/send_discord_post_to_telegram.py
+++ source/python/services/discord-to-telegram-forwarder/discord_to_telegram_forwarder/send_discord_post_to_telegram/send_discord_post_to_telegram.py
@@ -1,19 +1,12 @@
 import asyncio
-import random
 
-from typing import Any, Optional, Sequence, Union
+from typing import Sequence, Union
 
-from discord_to_telegram_forwarder.send_discord_post_to_telegram.get_shortened_url_from_tiny_url import (
-    get_shortened_url_from_tiny_url,
-)
-from discord_to_telegram_forwarder.send_discord_post_to_telegram.request_emoji_from_openai import (
-    request_emoji_from_openai,
+from discord_to_telegram_forwarder.send_discord_post_to_telegram.format_telegram_message_text import (
+    format_telegram_message_text,
 )
 from discord_to_telegram_forwarder.telegram_client import telegram_client
 from telethon import hints
-
-from lessmore.utils.file_helpers.read_file import read_file
-from lessmore.utils.path_helpers.get_current_dir import get_current_dir
 
 
 async def send_discord_post_to_telegram(
@@ -26,43 +19,21 @@ async def send_discord_post_to_telegram(
     add_inner_shortened_url: bool = True,  # add discord:// url, shortened and wrapped into https://
     files: Sequence[hints.FileLike] = (),  # from telethon
 ) -> None:
-    # - Read emoticons
-
-    emoticons = read_file(str(get_current_dir() / "emoticons.txt")).strip().split("\n")
-
-    # - Get emoji from openai
-
-    emoji = request_emoji_from_openai(f"{post_forum_channel_name} {post_title} {post_body}")
-
-    # - Make discord schema and shorten it to make it https:// with redirection to discord://
-
-    inner_shortened_url = (
-        get_shortened_url_from_tiny_url(post_url.replace("https", "discord")) if add_inner_shortened_url else ""
-    )
-
-    # - Format message for telegram
-
-    text = ""
-    if post_forum_channel_name:
-        text += f"#{post_forum_channel_name.replace('-', '_')}\n"
-
-    text += f"{emoji} **{post_title}**\n\n"
-
-    if post_body:
-        text += (
-            post_body[:3000] + ("" if len(post_body) < 3000 else "...") + "\n\n"
-        )  # maximum telegram message size is 4096. Making it 3000 to resever space for title and for the buffer
-
-    text += f"{post_author_name} {random.choice(emoticons)}\n"
-    text += f"[→ к посту]({post_url})"
-
-    if inner_shortened_url:
-        text += f"\n[→ к посту на apple-устройствах]({inner_shortened_url})"
-
     # - Send message to telegram
 
     await telegram_client.send_message(
-        entity=telegram_chat, message=text, parse_mode="md", link_preview=False, file=files or None
+        entity=telegram_chat,
+        message=format_telegram_message_text(
+            post_author_name=post_author_name,
+            post_body=post_body,
+            post_forum_channel_name=post_forum_channel_name,
+            post_title=post_title,
+            post_url=post_url,
+            add_inner_shortened_url=add_inner_shortened_url,
+        ),
+        parse_mode="md",
+        link_preview=False,
+        file=files or None,
     )
 
 
