4ca1a95d46248450756b08cb8a398ab8dda71004
marklidenberg <marklidenberg@gmail.com>
2023-09-28 22:21:45 +0400

    make more generic, change color

--- source/other/chrome-highlight-bookmarks/content.js
+++ source/other/chrome-highlight-bookmarks/content.js
@@ -1,8 +1,13 @@
-chrome.runtime.sendMessage({action: 'fetchBookmarks'}, (bookmarkLinks) => {
-  const pageLinks = document.querySelectorAll("a");
-  pageLinks.forEach((linkElement) => {
-    if (bookmarkLinks.includes(linkElement.href)) {
-      linkElement.style.backgroundColor = "yellow";
-    }
+chrome.runtime.sendMessage({ action: "fetchBookmarks" }, (bookmarkLinks) => {
+  const observer = new MutationObserver(() => {
+    const pageLinks = document.querySelectorAll("a");
+    pageLinks.forEach((linkElement) => {
+      const matchedBookmark = bookmarkLinks.find(bookmark => linkElement.href.startsWith(bookmark));
+      if (matchedBookmark) {
+        linkElement.style.color = "#186F65";
+      }
+    });
   });
+
+  observer.observe(document.body, { childList: true, subtree: true });
 });
