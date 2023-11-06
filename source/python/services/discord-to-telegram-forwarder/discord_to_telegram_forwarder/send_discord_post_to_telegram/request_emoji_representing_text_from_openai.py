1c00d28498a1405674f2400ae9976c8915942e0c
marklidenberg <marklidenberg@gmail.com>
2023-11-06 18:38:06 +0300

    feat(discord-to-telegram-forwarder): change default emojis limit to 1, update logic a bit

--- source/python/services/discord-to-telegram-forwarder/discord_to_telegram_forwarder/send_discord_post_to_telegram/request_emoji_representing_text_from_openai.py
+++ source/python/services/discord-to-telegram-forwarder/discord_to_telegram_forwarder/send_discord_post_to_telegram/request_emoji_representing_text_from_openai.py
@@ -1,10 +1,12 @@
+import time
+
 import emoji
 import openai
 
 from lessmore.utils.remove_duplicates_ordered import remove_duplicates_ordered
 
 
-def request_emoji_representing_text_from_openai(text: str, limit: int = 3) -> str:
+def request_emoji_representing_text_from_openai(text: str, limit: int = 1) -> str:
     """Returns emojis as a string (e.g. "👍")"""
 
     # - Get response text
@@ -14,15 +16,29 @@ def request_emoji_representing_text_from_openai(text: str, limit: int = 3) -> st
         messages=[
             {
                 "role": "system",
-                "content": """Your role as an Emoji Interpreter Model is to read and understand the core sentiments, ideas, and themes of short texts. Your insights should then be translated into a sequence of five relevant emojis, capturing the essence of the text in a visual and succinct way. Your work will enable emoji-based communication and understanding of textual data.""",
+                "content": """
+                Your role as an Emoji Interpreter Model is to read and understand the core sentiments, ideas, and themes of short texts. 
+                Your insights should then be translated into five emojis, each of which captures the essence of the text in a visual and succinct way. 
+                Your work will enable emoji-based communication and understanding of textual data.
+                Focus on the core ideas and sentiments of the text that make it stand out. 
+                """,
             },
             {
                 "role": "user",
-                "content": """Механики навигации в рабочем флоу by Mark Lidenberg
-        Хочу оптимизировать навигацию между разными сущностями при работе. Приложения, окна, вкладки, недавние, поиск по тексту, сессии, пины … - все вот это хочу разбить на рабочие механики и продумать, где что лучше как юзать""",
+                "content": """Кто делал себе вторую личность? Как правильно подготовить себя к заляганию на дно? by Georgy Gorbachev
+
+Я собрал определённый набор предметов для второй иностранной личности, однако как будто этого недостаточно. Хочу поделиться и проконсультироваться)))
+
+Также делаю себе advanved VPN cluster своими руками для впна как в РФ так и во вне для себя и семьи под прикрытием игрового сервиса, пробивающий будущую блокировку дипиайями большинство существующих VPN-протоколов. Обсудил бы это если останется время)""",
             },
-            {"role": "assistant", "content": """🔍💡🗺️🚀🤔"""},
-            {"role": "user", "content": f"### Text\n{text}"},
+            {
+                "role": "assistant",
+                "content": """🎭💼🕵🌐🔒""",
+            },
+            {
+                "role": "user",
+                "content": f"### Text\n{text}",
+            },
         ],
     )  # sample answer: {"id":"chatcmpl-7mS5ErOokrcpg33WsJKrZ6rnhLazt","object":"chat.completion","created":1691782052,"model":"gpt-3.5-turbo-0613","choices":[{"index":0,"message":{"role":"assistant","content":"☔"},"finish_reason":"stop"}],"usage":{"prompt_tokens":44,"completion_tokens":2,"total_tokens":46}} # pragma: allowlist secret
     response_text = response.choices[0].message.content
@@ -31,25 +47,56 @@ def request_emoji_representing_text_from_openai(text: str, limit: int = 3) -> st
 
     emojis = [letter for letter in response_text if emoji.is_emoji(letter)]
 
+    # - Move boring emojis to the end
+
+    emojis = list(
+        sorted(
+            emojis,
+            key=lambda emoji: emoji
+            in """👨😀😕😃😄😁😆😅🤣😂🙂🙃😉😊😇😍🥰😘😗😙😚😋😛😜😝🤑🤗🤔🤐🤨😐😑😶😏😒🙄😬🤥😌😔😪🤤😴😷🤒🤕🤮🤧🥴😵🤯😎🤓🧐😕😟🙁☹️😮😯😲😳🥺😦😧😨😰😥😢😭😱😖😣😞😓😩😫🥱😤😡😠🤬"""
+            + """👍👎👌👏👋🤚🖐✋🖖👌🤏✌️🤞🤟🤘👈👉👆👇☝️✊👊🤛🤜👏🙌👐🤲🙏🦶🦵💪🤝👐🫱🫲"""
+            + """👀🤷♂♀🤦🔍🔎❓❔💡✔💬🔥""",
+        )
+    )
+
     # - Remove duplicates
 
     emojis = remove_duplicates_ordered(emojis)
 
-    # - Limit
+    # - Return the first one
 
-    emojis = emojis[:limit]
-
-    # - Return
-
-    return "".join(emojis)
+    return "".join(emojis[:limit])
 
 
 def test():
-    print(
-        request_emoji_representing_text_from_openai(
-            "Yesterday I was walking in the street and I stepped in a puddle and my shoes got wet."
-        )
-    )
+    for text in [
+        """к слову про AI Search в своих файлах by Petr Lavrov
+
+https://www.theverge.com/2023/6/21/23767248/dropbox-ai-dash-universal-search""",
+        """А все знают, что значит вот это число в форварде поста?""",
+        """Попробовал DALLE-3 (через Bing), очень понравилось by Petr Lavrov
+
+https://www.bing.com/images/create/
+с Midjourney гораздо сложнее было сгенерить то что хочется
+""",
+        """Илья, а мне зашел birthdaycountbot :) Создает доброе ощущение sense of urgency by Mark Lidenberg
+
+Пока оставлю :) 
+
+https://t.me/birthdaycountbot
+
+Илья (Ilya)""",
+        """Кто делал себе вторую личность? Как правильно подготовить себя к заляганию на дно? by Georgy Gorbachev
+
+Я собрал определённый набор предметов для второй иностранной личности, однако как будто этого недостаточно. Хочу поделиться и проконсультироваться)))
+
+Также делаю себе advanved VPN cluster своими руками для впна как в РФ так и во вне для себя и семьи под прикрытием игрового сервиса, пробивающий будущую блокировку дипиайями большинство существующих VPN-протоколов. Обсудил бы это если останется время)""",
+        """ Хочу обсудить проблемы  недостатки и классные фичи приложений для изучения языков by Илья (Ilya)
+
+Разрабатываю прототип мобильного приложения, являющегося лучшей версией anki и хочу услышать об опыте взаимодействия с приложениями для изучения языков. Что понравилось, и чего не хватало""",
+    ]:
+        print(text[:10], request_emoji_representing_text_from_openai(text, limit=5))
+        time.sleep(1)
 
 
 if __name__ == "__main__":
