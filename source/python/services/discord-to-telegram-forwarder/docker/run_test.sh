3c1f70210c4f964cf2f97af8776f1854ace63b64
marklidenberg <marklidenberg@gmail.com>
2024-05-07 18:55:28 +0600

    fix(discord-to-telegram-forwarder): move to python 3.11.7 so that opencv works for arm64 build

--- source/python/services/discord-to-telegram-forwarder/docker/run_test.sh
+++ source/python/services/discord-to-telegram-forwarder/docker/run_test.sh
@@ -1,1 +1,3 @@
+docker stop discord-to-telegram-forwarder-test
+docker rm discord-to-telegram-forwarder-test
 docker run --detach --pull always --name discord-to-telegram-forwarder-test marklidenberg/discord-to-telegram-forwarder:latest python /monorepo/source/python/services/discord-to-telegram-forwarder/discord_to_telegram_forwarder/run.py --env test
