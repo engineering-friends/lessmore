9584e56c553bfbf4a242d8ef8668b2474e3f7dc1
marklidenberg <marklidenberg@gmail.com>
2024-05-15 15:56:26 +0600

    update

--- source/python/services/discord-to-telegram-forwarder/run_test.sh
+++ source/python/services/discord-to-telegram-forwarder/run_test.sh
@@ -8,6 +8,16 @@ poetry install
 
 ./../../../../git_secret/decrypt_secrets.sh
 
+
+# Get the current directory
+
+CURRENT_DIR=$(pwd)
+
+# Check if the current directory is in PYTHONPATH
+if [[ ":$PYTHONPATH:" != *":$CURRENT_DIR:"* ]]; then
+    export PYTHONPATH="$CURRENT_DIR:$PYTHONPATH"
+fi
+
 # - Run the service
 
 poetry run python discord_to_telegram_forwarder/main.py --env test
