f0021ae2aa22c2e45a05a6203ad24e6f17b558cc
marklidenberg <marklidenberg@gmail.com>
2024-05-24 00:46:09 +0600

    update

--- source/python/libs/lessmore.utils/lessmore/utils/functional/repeat_func/repeat_func.py
+++ source/python/libs/lessmore.utils/lessmore/utils/functional/repeat_func/repeat_func.py
@@ -2,13 +2,13 @@ from typing import *
 
 from more_itertools import repeatfunc as _repeatfunc, take as head
 
-from lessmore.utils.kwargs_to_args import kwargs_to_args
+from lessmore.utils.functional.repeat_func.kwargs_to_args import kwargs_to_args
 
 
 # todo later: better naming? [@marklidenberg]
 
 
-def _repeat_func(func: Callable, times: Optional[int], kwargs: Any) -> Iterable:
+def repeat_func(func: Callable, times: Optional[int], kwargs: Any) -> Iterable:
     return _repeatfunc(
         func,
         times,
@@ -20,7 +20,7 @@ def test():
     print(
         head(
             3,
-            _repeat_func(
+            repeat_func(
                 func=lambda x: x + 1,
                 times=None,
                 kwargs={"x": 1},
