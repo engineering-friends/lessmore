500e30daa1e9e79b5b2104a48f75befe3e14246b
marklidenberg <marklidenberg@gmail.com>
2024-06-01 22:55:35 +0600

    hotfix

--- source/python/services/discord-to-telegram-forwarder/run_test.sh
+++ source/python/services/discord-to-telegram-forwarder/run_test.sh
@@ -23,5 +23,5 @@ export PYTHONPATH="$MONOREPO_PATH/source/python/libs/lessmore.utils:$PYTHONPATH"
 #poetry run python discord_to_telegram_forwarder/run.py --env test
 
 # kill screen with name discord_to_telegram_forwarder_test
-screen -S discord_to_telegram_forwarder_test -X quit || true
+#screen -S discord_to_telegram_forwarder_test -X quit
 screen -dmS discord_to_telegram_forwarder_test  -L -Logfile logs/test.log poetry run python discord_to_telegram_forwarder/run.py --env test
