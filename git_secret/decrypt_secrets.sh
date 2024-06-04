f4034180bf02308a850adf2266770d555e7dc33a
marklidenberg <marklidenberg@gmail.com>
2024-06-04 19:53:11 +0600

    update

--- git_secret/decrypt_secrets.sh
+++ git_secret/decrypt_secrets.sh
@@ -1,2 +1,8 @@
 # Decrypt of *.secret files in the repository. This will create the decrypted files in the same directory with the same name but without the .secret extension.
-cd ${0%/*}/.. && git secret reveal -f # -f: force overwrite
+
+# - Go to repo root
+
+cd ${0%/*}/..
+
+# - Find all git secret files and decrypt them
+git secret reveal -fF # -f: force overwrite, -F: continue to reveal even if the file fails to decrypt (usually due to a missing .secret file)
--- git_secret/encrypt_secrets.sh
+++ git_secret/encrypt_secrets.sh
@@ -1,2 +1,51 @@
-# Encrypt all secrets in the repository. This will create encrypted files with the .secret suffix.
-cd ${0%/*}/.. && git secret hide
+
+# - Got to the root of the repository
+
+cd ${0%/*}/..
+
+# - Find all .secret files and add them to the git secret
+
+find . -type f -name "*.secret" | while read -r filename; do
+  echo "Adding secret :${filename%.secret}"
+  git secret add "${filename%.secret}"
+done
+
+# - Find all missing .secret files and remove them from git secret
+
+while IFS= read -r filename
+do
+  if [ ! -f "$filename.secret" ]; then
+    echo "file $filename.secret does not exist. Removing from git secret..."
+
+    # - Create a backup of $filename if it exists
+
+    if [ -f "$filename" ]; then
+      mv "$filename" "$filename.bak"
+    fi
+
+    # - Create stub files
+
+    touch "$filename"
+    touch "$filename.secret"
+
+    # - Remove secret
+
+    git secret remove "$filename"
+
+    # - Remove stub files
+
+    rm "$filename"
+    rm "$filename.secret"
+
+    # - Restore the backup of $filename if it exists
+    if [ -f "$filename.bak" ]; then
+      mv "$filename.bak" "$filename"
+    fi
+
+  fi
+done <<< "$(git secret list)"
+
+# - Encrypt all secrets in the repository
+
+git secret hide -F # -F forces hide to continue if a file to encrypt is missing. This will create encrypted filenames with the .secret suffix
+
