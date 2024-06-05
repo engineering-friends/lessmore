5b3edaaa25a592cefd86ae6c9685f505c2ae42f2
marklidenberg <marklidenberg@gmail.com>
2024-06-05 13:59:12 +0600

    update

--- source/python/services/palette/palette/aiogram_playground/elements/element.py
+++ source/python/services/palette/palette/aiogram_playground/elements/element.py
@@ -1,13 +1,18 @@
 from dataclasses import dataclass
 from typing import Optional
 
+from aiogram.types import InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from palette.aiogram_playground.elements.merge_keyboards import test
+
+
+callbacks = {}
 
 
 @dataclass
 class RenderedElement:
-    message: str = ""
-    keyboard_markup: Optional[InlineKeyboardBuilder] = None
+    text: str = ""
+    reply_markup: Optional[InlineKeyboardMarkup] = None
 
 
 class EmptyElement:
@@ -15,27 +20,20 @@ class EmptyElement:
         return RenderedElement()
 
 
-def merge_keyboards(keyboards):
-    merged_keyboard = InlineKeyboardBuilder()
-    for keyboard in keyboards:
-        for row in keyboard.as_markup().inline_keyboard:
-            for button in row:
-                merged_keyboard.button(text=button.text, callback_data=button.callback_data)
-    return merged_keyboard
+class ButtonElement:
+    def __init__(self, text: str):
+        self.text = text
 
+    def render(self) -> RenderedElement:
+        keyboard = InlineKeyboardBuilder()
 
-def test():
-    # Define two keyboards
-    keyboard1 = InlineKeyboardBuilder()
-    keyboard1.button(text="Go to Level 1", callback_data="go_to_level_1")
+        def _button_callback():
+            self.message = "Button clicked"
+            return self.render()
 
-    keyboard2 = InlineKeyboardBuilder()
-    keyboard2.button(text="Go to Level 2", callback_data="go_to_level_2")
-
-    merged_keyboard = merge_keyboards(keyboards=[keyboard1, keyboard2])
-
-    # Use the merged keyboard
-    print(merged_keyboard.as_markup())
+        callbacks["button"] = _button_callback
+        keyboard.button(text=self.text, callback_data="button")
+        return RenderedElement(text="", reply_markup=keyboard.as_markup())
 
 
 if __name__ == "__main__":
