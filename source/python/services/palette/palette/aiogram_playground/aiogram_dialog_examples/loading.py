33c05f46cc7198c3f38f7b60a2115782b7d6ab5e
marklidenberg <marklidenberg@gmail.com>
2024-06-05 15:27:58 +0600

    add more exmpales

---
+++ source/python/services/palette/palette/aiogram_playground/aiogram_dialog_examples/loading.py
@@ -0,0 +1,99 @@
+import asyncio
+import logging
+import os
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.storage.memory import MemoryStorage, SimpleEventIsolation
+from aiogram.types import CallbackQuery, Message
+from aiogram_dialog import BaseDialogManager, Dialog, DialogManager, StartMode, Window, setup_dialogs
+from aiogram_dialog.widgets.kbd import Button
+from aiogram_dialog.widgets.text import Const, Multi, Progress
+
+
+API_TOKEN = os.getenv("BOT_TOKEN")
+
+
+# name input dialog
+
+
+class Bg(StatesGroup):
+    progress = State()
+
+
+async def get_bg_data(dialog_manager: DialogManager, **kwargs):
+    return {
+        "progress": dialog_manager.dialog_data.get("progress", 0),
+    }
+
+
+bg_dialog = Dialog(
+    Window(
+        Multi(
+            Const("Your click is processing, please wait..."),
+            Progress("progress", 10),
+        ),
+        state=Bg.progress,
+        getter=get_bg_data,
+    ),
+)
+
+
+# main dialog
+class MainSG(StatesGroup):
+    main = State()
+
+
+async def start_bg(
+    callback: CallbackQuery,
+    button: Button,
+    manager: DialogManager,
+):
+    await manager.start(Bg.progress)
+    asyncio.create_task(background(callback, manager.bg()))
+
+
+async def background(callback: CallbackQuery, manager: BaseDialogManager):
+    count = 10
+    for i in range(1, count + 1):
+        await asyncio.sleep(1)
+        await manager.update(
+            {
+                "progress": i * 100 / count,
+            }
+        )
+    await asyncio.sleep(1)
+    await manager.done()
+
+
+main_menu = Dialog(
+    Window(
+        Const("Press button to start processing"),
+        Button(Const("Start"), id="start", on_click=start_bg),
+        state=MainSG.main,
+    ),
+)
+
+
+async def start(message: Message, dialog_manager: DialogManager):
+    await dialog_manager.start(MainSG.main, mode=StartMode.RESET_STACK)
+
+
+async def main():
+    # real main
+    logging.basicConfig(level=logging.INFO)
+    logging.getLogger("aiogram_dialog").setLevel(logging.DEBUG)
+    storage = MemoryStorage()
+    bot = Bot(token=API_TOKEN)
+    dp = Dispatcher(storage=storage, events_isolation=SimpleEventIsolation())
+    dp.include_router(bg_dialog)
+    dp.include_router(main_menu)
+
+    dp.message.register(start, CommandStart())
+    setup_dialogs(dp)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ source/python/services/palette/palette/aiogram_playground/aiogram_dialog_examples/simple.py
@@ -0,0 +1,177 @@
+import asyncio
+import logging
+import os.path
+
+from typing import Any
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart, ExceptionTypeFilter
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.storage.redis import DefaultKeyBuilder, RedisStorage
+from aiogram.types import CallbackQuery, ContentType, Message
+from aiogram_dialog import ChatEvent, Dialog, DialogManager, ShowMode, StartMode, Window, setup_dialogs
+from aiogram_dialog.api.exceptions import UnknownIntent, UnknownState
+from aiogram_dialog.widgets.input import MessageInput
+from aiogram_dialog.widgets.kbd import Back, Button, Row, Select, SwitchTo
+from aiogram_dialog.widgets.media import StaticMedia
+from aiogram_dialog.widgets.text import Const, Format, Multi
+from redis.asyncio.client import Redis
+
+
+src_dir = os.path.normpath(os.path.join(__file__, os.path.pardir))
+
+API_TOKEN = os.getenv("BOT_TOKEN")
+
+
+class DialogSG(StatesGroup):
+    greeting = State()
+    age = State()
+    finish = State()
+
+
+async def get_data(dialog_manager: DialogManager, **kwargs):
+    age = dialog_manager.dialog_data.get("age", None)
+    return {
+        "name": dialog_manager.dialog_data.get("name", ""),
+        "age": age,
+        "can_smoke": age in ("18-25", "25-40", "40+"),
+    }
+
+
+async def name_handler(
+    message: Message,
+    message_input: MessageInput,
+    manager: DialogManager,
+):
+    if manager.is_preview():
+        await manager.next()
+        return
+    manager.dialog_data["name"] = message.text
+    await message.answer(f"Nice to meet you, {message.text}")
+    await manager.next()
+
+
+async def other_type_handler(
+    message: Message,
+    message_input: MessageInput,
+    manager: DialogManager,
+):
+    await message.answer("Text is expected")
+
+
+async def on_finish(
+    callback: CallbackQuery,
+    button: Button,
+    manager: DialogManager,
+):
+    if manager.is_preview():
+        await manager.done()
+        return
+    await callback.message.answer("Thank you. To start again click /start")
+    await manager.done()
+
+
+async def on_age_changed(
+    callback: ChatEvent,
+    select: Any,
+    manager: DialogManager,
+    item_id: str,
+):
+    manager.dialog_data["age"] = item_id
+    await manager.next()
+
+
+dialog = Dialog(
+    Window(
+        Const("Greetings! Please, introduce yourself:"),
+        StaticMedia(
+            path=os.path.join(src_dir, "python_logo.png"),
+            type=ContentType.PHOTO,
+        ),
+        MessageInput(name_handler, content_types=[ContentType.TEXT]),
+        MessageInput(other_type_handler),
+        state=DialogSG.greeting,
+    ),
+    Window(
+        Format("{name}! How old are you?"),
+        Select(
+            Format("{item}"),
+            items=["0-12", "12-18", "18-25", "25-40", "40+"],
+            item_id_getter=lambda x: x,
+            id="w_age",
+            on_click=on_age_changed,
+        ),
+        state=DialogSG.age,
+        getter=get_data,
+        preview_data={"name": "Tishka17"},
+    ),
+    Window(
+        Multi(
+            Format("{name}! Thank you for your answers."),
+            Const("Hope you are not smoking", when="can_smoke"),
+            sep="\n\n",
+        ),
+        Row(
+            Back(),
+            SwitchTo(Const("Restart"), id="restart", state=DialogSG.greeting),
+            Button(Const("Finish"), on_click=on_finish, id="finish"),
+        ),
+        getter=get_data,
+        state=DialogSG.finish,
+    ),
+)
+
+
+async def start(message: Message, dialog_manager: DialogManager):
+    # it is important to reset stack because user wants to restart everything
+    await dialog_manager.start(DialogSG.greeting, mode=StartMode.RESET_STACK)
+
+
+async def on_unknown_intent(event, dialog_manager: DialogManager):
+    # Example of handling UnknownIntent Error and starting new dialog.
+    logging.error("Restarting dialog: %s", event.exception)
+    await dialog_manager.start(
+        DialogSG.greeting,
+        mode=StartMode.RESET_STACK,
+        show_mode=ShowMode.SEND,
+    )
+
+
+async def on_unknown_state(event, dialog_manager: DialogManager):
+    # Example of handling UnknownState Error and starting new dialog.
+    logging.error("Restarting dialog: %s", event.exception)
+    await dialog_manager.start(
+        DialogSG.greeting,
+        mode=StartMode.RESET_STACK,
+        show_mode=ShowMode.SEND,
+    )
+
+
+async def main():
+    # real main
+    logging.basicConfig(level=logging.INFO)
+    bot = Bot(token=API_TOKEN)
+
+    storage = RedisStorage(
+        Redis(),
+        # in case of redis you need to configure key builder
+        key_builder=DefaultKeyBuilder(with_destiny=True),
+    )
+    dp = Dispatcher(storage=storage)
+    dp.message.register(start, CommandStart())
+    dp.errors.register(
+        on_unknown_intent,
+        ExceptionTypeFilter(UnknownIntent),
+    )
+    dp.errors.register(
+        on_unknown_state,
+        ExceptionTypeFilter(UnknownState),
+    )
+    dp.include_router(dialog)
+    setup_dialogs(dp)
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
