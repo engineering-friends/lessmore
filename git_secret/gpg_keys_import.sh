de19819209953aeaf142d0f950e12d8e2bc648f6
marklidenberg <marklidenberg@gmail.com>
2024-06-16 23:00:38 +0600

    update secrets key

--- git_secret/gpg_keys_import.sh
+++ git_secret/gpg_keys_import.sh
@@ -6,23 +6,23 @@ cd ${0%/*}
 
 git secret init
 
-# - If there are already gpg keys imported then exit. Check that F123DBA179C7A29B79F1596352BD4E130D6980FA key is imported with gpg --list-keys
+# - If there are already gpg keys imported then exit. Check that AE448D555EF175712DBBBFCCABF36667ED4DED6E key is imported with gpg --list-keys
 
 #[keyboxd]
 #---------
 #pub   ed25519 2023-08-11 [SC]
-#      F123DBA179C7A29B79F1596352BD4E130D6980FA
+#      AE448D555EF175712DBBBFCCABF36667ED4DED6E
 #uid           [ unknown] Mark Lidenberg (lessmore-repo) <marklidenberg@gmail.com>
 #sub   cv25519 2023-08-11 [E]
 
-if gpg --list-keys | grep -q F123DBA179C7A29B79F1596352BD4E130D6980FA; then
+if gpg --list-keys | grep -q AE448D555EF175712DBBBFCCABF36667ED4DED6E; then
     echo "GPG keys already imported"
     exit 0
 fi
 
 # - Get the keys from enrcyped file. Ask admin for the password
 
-unzip files/gpg_keys.zip
+unzip gpg_keys.zip
 mv gpg_keys/* .
 rm -rf gpg_keys
 
