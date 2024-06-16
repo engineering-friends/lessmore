de87fc587f0580004861d5e5c8ed633d94ceeaa8
marklidenberg <marklidenberg@gmail.com>
2024-06-16 21:39:17 +0600

    update

--- source/python/services/archive/2024-06 release-lessmore-service-example/release.sh
+++ source/python/services/archive/2024-06 release-lessmore-service-example/release.sh
@@ -10,7 +10,7 @@ git rev-parse --abbrev-ref HEAD | grep '^master$' > /dev/null || { echo "Current
 
 # - Exit if there are staged changes
 
-git diff --staged --quiet || { echo "There are staged changes. Exiting."; exit 1; }
+git diff --cached --name-only && { echo "There are staged changes. Exiting."; exit 1; }
 
 # - Exit if there are unpulled commits
 
