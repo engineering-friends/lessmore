f703b5231a1aa440fe1f1f7e6034d5d7cfe0fc61
marklidenberg <marklidenberg@gmail.com>
2024-06-17 11:13:01 +0600

    update

---
+++ source/python/libs/lessmore.utils/lessmore/utils/combo_formatter/__main__.py
@@ -0,0 +1,51 @@
+import os
+
+import fire
+
+from lessmore.utils.file_primitives.list_files import list_files
+from lessmore.utils.loguru_utils.setup_json_loguru import setup_json_loguru
+from lessmore.utils.read_config.read_config import read_config
+from lessmore.utils.system.run_system_command import run_system_command
+from loguru import logger
+
+
+config = read_config(source="config.yaml")
+
+
+def format_file(filename: str) -> None:
+    # - Get file type
+
+    if "Dockerfile" in filename:
+        file_type = "Dockerfile"
+    else:
+        file_type = os.path.splitext(filename)[-1]
+
+    # - Apply formatters
+
+    for group, values in config.items():
+        if file_type not in values["file_types"]:
+            continue
+
+        for formatter in values["formatters"]:
+            logger.debug(
+                "Running formatter on file",
+                formatter=formatter["name"],
+                filename=filename,
+                command=formatter["command"].format(filename=filename),
+            )
+            run_system_command(
+                command=formatter["command"].format(filename=filename),
+                working_dir=formatter.get("working_dir"),
+            )
+
+
+def main(*sources) -> None:
+    assert len(sources) > 0, "Specify sources"
+    for source in sources:
+        for filename in list_files(source):
+            format_file(filename)
+
+
+if __name__ == "__main__":
+    setup_json_loguru()
+    fire.Fire(main)
