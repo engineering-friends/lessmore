6d3bdb24e10a503d0beaef35bd44fec79503337f
marklidenberg <marklidenberg@gmail.com>
2024-06-22 21:12:00 +0600

    update

--- source/python/services/palette/palette/teledo/examples/button_increment.py
+++ source/python/services/palette/palette/teledo/examples/button_increment.py
@@ -11,10 +11,12 @@ from palette.teledo.start_polling.start_polling import start_polling
 async def command_starter(message: Message, interaction: Interaction) -> None:
     async def _callback(message: Message, root: Element, element: ButtonElement):
         element.text = str(int(element.text) + 1)
+        await message.answer("Incremented by 1")
         return await root(interaction=interaction)
 
     async def _message_callback(message: Message, root: Element):
         root.text = str(int(root.text) + 10)
+        await message.answer("Incremented by 10")
         return await root(interaction=interaction)
 
     await ButtonElement(text="0", callback=_callback, message_callback=_message_callback)(
