8c53e3e6a1c0f0ffe38c5e9f4ab099ad0e56228d
marklidenberg <marklidenberg@gmail.com>
2024-07-22 18:18:54 +0600

    update

--- source/python/services/playground/notion-database-ai/notion_database_ai/get_properties_to_attribute_name.py
+++ source/python/services/playground/notion-database-ai/notion_database_ai/get_properties_to_attribute_name.py
@@ -3,8 +3,8 @@ import inspect
 from dataclasses import dataclass
 from typing import Any
 
-from notion_database_ai.columns.auto_property import auto_property
-from notion_database_ai.columns.input_property import input_property
+from notion_database_ai.properties.auto_property import auto_property
+from notion_database_ai.properties.input_property import input_property
 
 
 def get_property_name_to_attribute_name(row: Any) -> dict:
