00ff4b66ef949e8f00dd7cff8d9aefbb09397b03
marklidenberg <marklidenberg@gmail.com>
2024-07-22 22:40:09 +0600

    update

--- source/python/services/playground/notion-database-ai/notion_database_ai/fields/field.py
+++ source/python/services/playground/notion-database-ai/notion_database_ai/fields/field.py
@@ -1,11 +1,11 @@
-from dataclasses import field
+from dataclasses import field as dataclass_field
 
 from lessmore.utils.functional.dict.drop import drop
 
 
-def input_property(name: str, **kwargs):
+def field(name: str, **kwargs):
     # syntax sugar to add field with adding column name to metadata
-    return field(
+    return dataclass_field(
         **drop(kwargs, ["metadata"]),
         metadata={
             "property_name": name,
