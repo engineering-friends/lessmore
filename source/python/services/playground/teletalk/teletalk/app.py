c0a3b01768ce8f8e4ca8d70795b9ce52c97cef37
marklidenberg <marklidenberg@gmail.com>
2024-09-08 23:10:51 +0600

    init new teletalk structure

--- source/python/services/playground/teletalk/teletalk/app.py
+++ source/python/services/playground/teletalk/teletalk/app.py
@@ -1,11 +1,18 @@
-from typing import Callable
+from typing import Callable, Optional
+
+import aiogram
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.types import BotCommand, CallbackQuery, Message
 
 
-class TeletalkApp:
+class App:
     def __init__(self):
         self.talks = []
 
-    def start_new_talk(self, starter: Callable, starter_kwargs: dict):
+    async def start_new_talk(self, starter: Callable, starter_kwargs: dict):
         # - Create talk and add it to the list
 
         pass
@@ -18,7 +25,7 @@ class TeletalkApp:
 
         pass
 
-    def on_ui_event(self, message):
+    async def on_callback_query(self, callback_query: CallbackQuery) -> None:
         # - Find the talk by message_id
 
         pass
@@ -27,7 +34,7 @@ class TeletalkApp:
 
         pass
 
-    def on_user_message(self, message):
+    async def on_message(self, message: Message) -> None:
         # - Send the message to the buffer of the chat
 
         pass
@@ -48,11 +55,35 @@ class TeletalkApp:
 
         pass
 
-    def start_polling(self, starter: Callable):
-        # - Configure global handlers (UI and text)
+    async def start_polling(
+        self,
+        bot: Bot | str,
+        default_bot_properties: DefaultBotProperties = DefaultBotProperties(parse_mode=ParseMode.HTML),
+        commands: Optional[list[BotCommand]] = None,  # description of commands
+    ) -> None:
+        # - Init dispatcher
 
-        pass
+        dp = Dispatcher()
+
+        # - Register handlers
+
+        dp.callback_query.register(self.on_callback_query)
+        dp.message.register(self.on_message)
+
+        # - Init bot from token if needed
+
+        if isinstance(bot, str):
+            bot = Bot(
+                token=bot,
+                default=default_bot_properties,
+            )
+
+        # - Set commands for bot
+
+        # it's possible take from command_starter docs, but `bot_set_my_commands` might takes a while (like ~1s). For not waiting every test, it's just easier to pass it as an argument directly and omit it for tests
+        if commands is not None:
+            await bot.set_my_commands(commands=commands)
 
         # - Start polling
 
-        pass
+        await dp.start_polling(bot)
