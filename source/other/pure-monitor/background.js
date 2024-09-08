7cc865b9587468113db079af85e426d1c1e1ffbc
marklidenberg <marklidenberg@gmail.com>
2024-09-08 15:34:20 +0600

    chore(airflow-etl.hhpoker_tables_mining): update start_at

--- source/other/pure-monitor/background.js
+++ source/other/pure-monitor/background.js
@@ -1,14 +1,20 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete") {
-    chrome.scripting.executeScript({
-      target: {tabId: tabId},
-      function: reloadPage
-    });
-  }
+    if (changeInfo.status === "complete") {
+        chrome.scripting.executeScript({
+            target: {tabId: tabId},
+            function: reloadPage
+        });
+    }
 });
 
 function reloadPage() {
-  setInterval(() => {
-    location.reload();
-  }, (Math.floor(Math.random() * (75 - 45)) + 45) * 1000);  // Reload every 60 seconds on average
+    chrome.storage.local.get(['hasFired'], function (result) {
+        hasFired = result.hasFired || false; // Default to false if no value is stored
+
+        if (!hasFired) {
+            setInterval(() => {
+                location.reload();
+            }, (Math.floor(Math.random() * (75 - 45)) + 45) * 1000);  // Reload every 60 seconds on average
+        }
+    });
 }
