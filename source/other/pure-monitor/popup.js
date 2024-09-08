303935f4c1d9023271fad17757e67e3818e76f55
marklidenberg <marklidenberg@gmail.com>
2024-09-08 22:06:18 +0600

    chore(airflow-etl.hhpoker_tables_mining): update start_at

--- source/other/pure-monitor/popup.js
+++ source/other/pure-monitor/popup.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // Send a message to the content script to reset the flag
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {action: "resetHasFired"});
+            chrome.tabs.reload(tabs[0].id);
         });
     });
 });
