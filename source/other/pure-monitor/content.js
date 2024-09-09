f7afd5350f67ffec442694d3f2946ac27c8d41ab
marklidenberg <marklidenberg@gmail.com>
2024-09-09 22:47:32 +0600

    update

--- source/other/pure-monitor/content.js
+++ source/other/pure-monitor/content.js
@@ -1,9 +1,6 @@
 // content.js runs for every tab that matches the URL patterns in manifest.json
 
 
-
-// globalThis.sendNotification('Content worker started');
-
 // - Get hasFired from storage or set it to false by default
 
 let hasFired = false;
@@ -14,77 +11,65 @@ chrome.storage.local.get(['hasFired'], function (result) {
 });
 
 
+function fireEventAndNotify(message) {
+    chrome.storage.local.set({hasFired: true}, function () {
+        console.log('hasFired value stored as true.');
+    });
+    globalThis.sendNotification(message);
+}
+
 // - Add listener to check 5 seconds after the page loads
 
 window.addEventListener('load', () => {
     // wait for 5 seconds for the page to load and then check the conditions
-    console.log('Checking conditions...');
     setTimeout(() => {
         // - Return if already fired
+
         if (hasFired) return;
 
         // - Get URL
+
         const url = window.location.href;
 
-        console.log('URL:', url);
-        //
-        // // - Pure
-        // if (url.includes('pure.app')) {
-        //     // check for a new message (disabled for now)
-        //     if (document.querySelector('div.sc-cepbVR')) { // black dot of unread message
-        //         fireEventAndNotify("New message");
-        //         return;
-        //     }
-        //
-        //     const elements = document.querySelectorAll('div.sc-jzNkva'); // posts
-        //
-        //     for (let element of elements) {
-        //         const lastSeenSpans = element.querySelectorAll('span.sc-imwsjW span'); // 6km, online
-        //
-        //         if (!element.textContent.includes('Gift sent')) {
-        //             let distance = 0;
-        //             let lastSeenStatus = '';
-        //
-        //             // Get the distance and last seen status
-        //             if (lastSeenSpans.length >= 2) {
-        //                 distance = parseFloat(lastSeenSpans[0].textContent);
-        //                 lastSeenStatus = lastSeenSpans[1].textContent.trim();
-        //             }
-        //
-        //             // Check if distance is less than 40 and last seen status is "online"
-        //             if (distance < 40 && lastSeenStatus.toLowerCase() === 'online') {
-        //                 fireEventAndNotify("New user");
-        //                 break;
-        //             }
-        //         }
-        //     }
-        // }
-        //
-        // // - Bumble
-        // // check if 'body' has text "Want to keep matching?"
-        // if (url.includes('bumble.com')) {
-        //     if (!document.querySelector('body').textContent.includes('Want to keep matching?')) {
-        //         fireEventAndNotify("New animal");
-        //     }
-        // }
-        //
-        // // A helper function to set hasFired and send notification
-        // function fireEventAndNotify(message) {
-        //     hasFired = true;
-        //     chrome.storage.local.set({ hasFired: true }, function () {
-        //         console.log('hasFired value stored as true.');
-        //     });
-        //     sendNotification(message);
-        // }
+        // - Pure
+
+        if (url.includes('pure.app')) {
+            // check for a new message (disabled for now)
+            if (document.querySelector('div.sc-cepbVR')) { // black dot of unread message
+                fireEventAndNotify("New message");
+                return;
+            }
+
+            const elements = document.querySelectorAll('div.sc-jzNkva'); // posts
+
+            for (let element of elements) {
+                const lastSeenSpans = element.querySelectorAll('span.sc-imwsjW span'); // 6km, online
+
+                if (!element.textContent.includes('Gift sent')) {
+                    let distance = 0;
+                    let lastSeenStatus = '';
+
+                    // Get the distance and last seen status
+                    if (lastSeenSpans.length >= 2) {
+                        distance = parseFloat(lastSeenSpans[0].textContent);
+                        lastSeenStatus = lastSeenSpans[1].textContent.trim();
+                    }
+
+                    // Check if distance is less than 40 and last seen status is "online"
+                    if (distance < 40 && lastSeenStatus.toLowerCase() === 'online') {
+                        fireEventAndNotify("New user");
+                        break;
+                    }
+                }
+            }
+        }
+
+        // - Bumble
+        // check if 'body' has text "Want to keep matching?"
+        if (url.includes('bumble.com')) {
+            if (!document.querySelector('body').textContent.includes('Want to keep matching?')) {
+                fireEventAndNotify("New animal");
+            }
+        }
     }, 5000); // 5 second delay
 });
-
-// A helper function to set hasFired and send notification
-function fireEventAndNotify(message) {
-    hasFired = true;
-    chrome.storage.local.set({ hasFired: true }, function () {
-        console.log('hasFired value stored as true.');
-    });
-    sendNotification(message);
-}
-
