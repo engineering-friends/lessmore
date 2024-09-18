c02d704b8a1e2cbbcae9b4b2c5139ffb97c52800
marklidenberg <marklidenberg@gmail.com>
2024-09-19 01:58:40 +0600

    update

---
+++ source/python/services/ef-bots/ef_bots/ef_org_bot/deps/deps.py
@@ -0,0 +1,37 @@
+import os
+
+from dataclasses import dataclass
+from pathlib import Path
+
+import openai
+
+from ef_bots.ef_org_bot.deps.config.config import Config
+from lessmore.utils.loguru_utils.setup_json_loguru import setup_json_loguru
+from lessmore.utils.read_config.read_config import read_config
+
+
+@dataclass
+class TestDeps:
+    config: Config
+
+    @staticmethod
+    def load(log_level="DEBUG") -> "Deps":
+        # - Setup loguru
+
+        setup_json_loguru(level=log_level)
+
+        # - Init config
+
+        config = Config(**read_config([f"{str(Path(__file__).parent)}/config/config.yaml"]))
+
+        # - Return deps
+
+        return TestDeps(config=config)
+
+
+def test():
+    print(TestDeps.load().config)
+
+
+if __name__ == "__main__":
+    test()
