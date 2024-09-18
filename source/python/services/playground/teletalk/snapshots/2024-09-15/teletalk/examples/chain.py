fc6d1663612771ce5d0c447d16814d1949c649c9
marklidenberg <marklidenberg@gmail.com>
2024-09-19 01:51:42 +0600

    add external callback processing

--- source/python/services/playground/teletalk/snapshots/2024-09-15/teletalk/examples/chain.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-
-from teletalk.models.response import Response
-
-
-async def starter(response: Response):
-    age = await response.ask(text="How old are you?")
-    name = await response.ask(text="What is your name?")
-    await response.tell(text=f"Hello, {name}! You are {age} years old.")
