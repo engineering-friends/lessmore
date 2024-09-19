5ecf9e37f937661142485f5245db5614e51ae314
marklidenberg <marklidenberg@gmail.com>
2024-09-19 18:04:13 +0600

    update

--- source/python/services/ef-bots/ef_bots/ef_threads/playground/get_message_views.py
+++ source/python/services/ef-bots/ef_bots/ef_threads/playground/get_message_views.py
@@ -2,8 +2,17 @@ import asyncio
 
 from ef_bots.ef_threads.deps.deps import Deps
 from telethon.sync import TelegramClient
-from telethon.tl.functions.messages import GetMessagesViewsRequest
-from telethon.tl.types import InputChannel, InputMessageID
+from telethon.tl.functions.messages import GetMessageReadParticipantsRequest, GetMessagesViewsRequest
+from telethon.tl.types import (
+    InputChannel,
+    InputMessageID,
+    InputPeerChannel,
+    InputPeerChat,
+    InputPeerUser,
+    PeerChannel,
+    PeerChat,
+    ReadParticipantDate,
+)
 
 
 def test():
@@ -18,19 +27,21 @@ def test():
 
         # - Get message views
 
-        # Get the input channel (replace channel_id with the real channel id or username)
-        channel = await deps.telegram_user_client.get_entity(-1001897462358)  # EF Test
+        group = await deps.telegram_user_client.get_entity(-1001980795916)  # EF Test
 
-        # Fetch the views for the message (replace message_id with the message's id)
-        result = await deps.telegram_user_client(
-            GetMessagesViewsRequest(
-                peer=channel,
-                id=[2],  # You can pass multiple message IDs in this list
-                increment=False,  # Set this to True if you want to increase the view count
+        # - Get last message in the group
+
+        messages = await deps.telegram_user_client.get_messages(group, limit=1)
+
+        result: list[ReadParticipantDate] = await deps.telegram_user_client(
+            GetMessageReadParticipantsRequest(
+                peer=group,
+                msg_id=messages[0].id,
             )
         )
 
-        pass
+        for value in result:
+            print(value)
 
     asyncio.run(main())
 
