9428c661b5a3f1c93c82a9b27589f99a39cd1311
marklidenberg <marklidenberg@gmail.com>
2024-09-21 01:27:36 +0600

    user_states

--- source/python/services/simple-scheduler/simple_scheduler/deps/config/config.py
+++ source/python/services/simple-scheduler/simple_scheduler/deps/config/config.py
@@ -4,6 +4,13 @@ from pydantic_settings import BaseSettings
 
 
 class Config(BaseSettings):
+    # - Telegram
+
+    telegram_api_id: str
+    telegram_api_hash: str
     telegram_bot_name: str
     telegram_bot_token: str
+
+    # - Notion
+
     notion_token: str
--- source/python/services/simple-scheduler/simple_scheduler/run_prod.sh
+++ source/python/services/simple-scheduler/simple_scheduler/run_prod.sh
@@ -32,5 +32,6 @@ export PYTHONPATH="$MONOREPO_PATH/source/python/services/playground/telethon-pla
 # - Run the service in screen
 
 screen -X -S simple_scheduler quit
-screen -S simple_scheduler -L -Logfile logs/prod.log uv run python main.py --env prod # `ctrl+a d` to detach
+#screen -S simple_scheduler -L -Logfile logs/prod.log uv run python main.py --env prod # `ctrl+a d` to detach
+uv run python main.py --env prod # `ctrl+a d` to detach
 #screen -S ef_bot_org uv run python main.py # macos
