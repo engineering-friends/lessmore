a55fed08ce498ea2be2f90b4510b7818b28c619c
marklidenberg <marklidenberg@gmail.com>
2024-09-21 01:18:52 +0600

    user_states

---
+++ source/python/services/simple-scheduler/run_prod.sh
@@ -0,0 +1,36 @@
+# - Go to the service directory
+
+cd ${0%/*}
+git pull
+
+# - Install uv if not installed
+
+uv --version || curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.cargo/env
+
+# - Sync uv
+
+uv sync
+
+# Get directories
+
+SERVICE_PATH=$(pwd)/../..
+BOT_PATH=$(pwd)
+MONOREPO_PATH=$(echo $SERVICE_PATH | sed -E 's/(.*)\/source\/.*/\1/') # Crop /a/b/c/.../lessmore/source/... -> /a/b/c/.../lessmore
+
+# - Decrypt secrets
+
+$MONOREPO_PATH/git_secret/decrypt_secrets.sh
+
+# - Set PYTHONPATH
+
+export PYTHONPATH="$SERVICE_PATH:$PYTHONPATH"
+export PYTHONPATH="$MONOREPO_PATH/source/python/libs/lessmore.utils:$PYTHONPATH"
+export PYTHONPATH="$MONOREPO_PATH/source/python/libs/teletalk:$PYTHONPATH"
+export PYTHONPATH="$MONOREPO_PATH/source/python/services/playground/telethon-playground:$PYTHONPATH"
+
+# - Run the service in screen
+
+screen -X -S simple_scheduler quit
+screen -S simple_scheduler -L -Logfile logs/prod.log uv run python main.py --env prod # `ctrl+a d` to detach
+#screen -S ef_bot_org uv run python main.py # macos
--- source/python/services/simple-scheduler/simple_scheduler/deps/config/config.py
+++ source/python/services/simple-scheduler/simple_scheduler/deps/config/config.py
@@ -4,14 +4,6 @@ from pydantic_settings import BaseSettings
 
 
 class Config(BaseSettings):
-    # - Telegram
-
-    telegram_api_id: str
-    telegram_api_hash: str
     telegram_bot_name: str
     telegram_bot_token: str
-    telegram_ef_chats: dict[str, Union[int, str]]
-
-    # - Notion
-
     notion_token: str
