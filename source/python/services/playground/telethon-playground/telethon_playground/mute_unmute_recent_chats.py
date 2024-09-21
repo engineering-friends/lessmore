845b927f3545596ce9e8e3a66cacf3e7ed8ee72d
marklidenberg <marklidenberg@gmail.com>
2024-09-21 15:42:40 +0600

    update

--- source/python/services/playground/telethon-playground/telethon_playground/mute_unmute_recent_chats.py
+++ source/python/services/playground/telethon-playground/telethon_playground/mute_unmute_recent_chats.py
@@ -4,21 +4,22 @@ from datetime import datetime, timedelta
 
 from lessmore.utils.to_anything.to_datetime import to_datetime
 from loguru import logger
-from telethon import TelegramClient
+from telethon import TelegramClient, events, types
+from telethon.tl import functions
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from telethon.tl.functions.messages import GetHistoryRequest
 from telethon.tl.types import InputPeerNotifySettings
 from telethon_playground.deps.deps import Deps
 
 
-async def mute_unrecent_chats(telegram_client: TelegramClient, offset: timedelta = timedelta(hours=4)):
+async def mute_unrecent_chats(client: TelegramClient, offset: timedelta = timedelta(hours=4)):
     # - Get my id
 
-    me = await telegram_client.get_me()
+    me = await client.get_me()
 
     # - Process chats
 
-    async for dialog in telegram_client.iter_dialogs():
+    async for dialog in client.iter_dialogs():
         logger.info(f"Processing chat: {dialog.title}")
 
         # - Skip muted chats
@@ -33,7 +34,7 @@ async def mute_unrecent_chats(telegram_client: TelegramClient, offset: timedelta
 
         # -- Get message that was sent 4 hours ago
 
-        history = await telegram_client(
+        history = await client(
             GetHistoryRequest(
                 peer=dialog.entity,
                 limit=1,
@@ -52,7 +53,7 @@ async def mute_unrecent_chats(telegram_client: TelegramClient, offset: timedelta
 
         # -- Get the chat history
 
-        history = await telegram_client(
+        history = await client(
             GetHistoryRequest(
                 peer=dialog.entity,
                 limit=0,
@@ -85,7 +86,7 @@ async def mute_unrecent_chats(telegram_client: TelegramClient, offset: timedelta
 
         # - Mute the chat indefinitely
 
-        await telegram_client(
+        await client(
             UpdateNotifySettingsRequest(
                 peer=dialog.entity,
                 settings=InputPeerNotifySettings(mute_until=2**31 - 1),  # Mute indefinitely
@@ -96,7 +97,48 @@ async def mute_unrecent_chats(telegram_client: TelegramClient, offset: timedelta
 
 def test():
     async def main():
-        await mute_unrecent_chats(await Deps.load(env="test").started_telegram_user_client())
+        # - Init deps
+
+        deps = Deps.load(env="test")
+
+        # - Get telegram client
+
+        client = await deps.telegram_user_client
+
+        # - Define unmute handler
+
+        @client.on(events.NewMessage(incoming=True))
+        async def handler(event):
+            # Get the chat object where the message was sent
+            chat = await event.get_chat()
+
+            if chat.muted:  # Check if the chat is muted
+                # Unmute the chat
+                await client(
+                    functions.account.UpdateNotifySettingsRequest(
+                        peer=chat,
+                        settings=types.InputPeerNotifySettings(mute_until=None),
+                    )
+                )
+
+                logger.info(f'Chat "{chat.title}" has been unmuted.')
+
+        # - Start the client
+
+        await client.start()
+
+        # - Spawn muting task indefinitely in a loop
+
+        async def muting_loop():
+            while True:
+                await mute_unrecent_chats(client)
+                await asyncio.sleep(300)
+
+        asyncio.create_task(muting_loop())
+
+        # - Start polling
+
+        await client.run_until_disconnected()
 
     asyncio.run(main())
 
