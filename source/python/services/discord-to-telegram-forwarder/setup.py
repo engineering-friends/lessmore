a27043b51bc436d29e0d1ffd9934fa91ec645919
marklidenberg <marklidenberg@gmail.com>
2024-09-25 21:11:42 +0600

    fix

---
+++ source/python/services/discord-to-telegram-forwarder/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+
+# Read the content of the README.md file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="discord-to-telegram-forwarder",
+    version="0.1.2",
+    author="marklidenberg",
+    author_email="marklidenberg@gmail.com",
+    description="A Discord to Telegram forwarder.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    python_requires=">=3.9",
+)
