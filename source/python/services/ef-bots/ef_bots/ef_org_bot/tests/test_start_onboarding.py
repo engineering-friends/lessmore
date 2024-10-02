3c4f53ad8cccba61f5f6018c909fe5597e543ed6
marklidenberg <88275928+marklidenberg@users.noreply.github.com>
2024-10-02 17:02:06 +0600

    feat(ef-bot-org): better flow (#50)

--- source/python/services/ef-bots/ef_bots/ef_org_bot/tests/test_start_onboarding.py
+++ source/python/services/ef-bots/ef_bots/ef_org_bot/tests/test_start_onboarding.py
@@ -4,8 +4,8 @@ from ef_bots.ef_org_bot.main import EfOrgBot
 
 
 async def test_start_onboarding():
-    async with EfOrgBot().stack() as (ef_org_bot, app):
-        await app.run(starters={ef_org_bot.config.telegram_test_chat_id: ef_org_bot.start_onboarding})
+    async with EfOrgBot.stack(env="test") as (ef_org_bot, app):
+        await app.run(starters={ef_org_bot.deps.config.telegram_test_chat_id: ef_org_bot.start_onboarding})
 
 
 if __name__ == "__main__":
