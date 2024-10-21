ee9da8a7a6cba3a090a88a52feb65b0ffe17a3dd
marklidenberg <marklidenberg@gmail.com>
2024-10-21 21:20:45 +0600

    update

--- source/other/safari-bookmarks-highlighter/Shared (Extension)/Resources/background.js
+++ source/other/safari-bookmarks-highlighter/Shared (Extension)/Resources/background.js
@@ -3,4 +3,24 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
     if (request.greeting === "hello")
         return Promise.resolve({ farewell: "goodbye" });
+
+    if (request.action === "getBookmarks") {
+        return browser.bookmarks.getTree().then((bookmarkTreeNodes) => {
+            const bookmarks = flattenBookmarks(bookmarkTreeNodes);
+            return { bookmarks: bookmarks.slice(0, 5) };
+        });
+    }
 });
+
+function flattenBookmarks(bookmarkTreeNodes) {
+    let bookmarks = [];
+    for (const node of bookmarkTreeNodes) {
+        if (node.url) {
+            bookmarks.push({ title: node.title, url: node.url });
+        }
+        if (node.children) {
+            bookmarks = bookmarks.concat(flattenBookmarks(node.children));
+        }
+    }
+    return bookmarks;
+}
--- source/other/safari-bookmarks-highlighter/Shared (Extension)/Resources/popup.js
+++ source/other/safari-bookmarks-highlighter/Shared (Extension)/Resources/popup.js
@@ -1,1 +1,18 @@
-console.log("Hello World!", browser);
+document.addEventListener('DOMContentLoaded', function () {
+    browser.runtime.sendMessage({ action: "getBookmarks" })
+        .then((response) => {
+            const bookmarkList = document.getElementById('bookmarkList');
+            response.bookmarks.forEach((bookmark) => {
+                const li = document.createElement('li');
+                const a = document.createElement('a');
+                a.href = bookmark.url;
+                a.textContent = bookmark.title;
+                a.target = '_blank';
+                li.appendChild(a);
+                bookmarkList.appendChild(li);
+            });
+        })
+        .catch((error) => {
+            console.error('Error fetching bookmarks:', error);
+        });
+});
