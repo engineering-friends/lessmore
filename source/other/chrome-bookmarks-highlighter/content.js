f7ab565ff38b2ef7ba1e89119fde2e594ac94a95
marklidenberg <marklidenberg@gmail.com>
2024-10-21 22:53:37 +0600

    rollback

--- source/other/chrome-bookmarks-highlighter/content.js
+++ source/other/chrome-bookmarks-highlighter/content.js
@@ -12,9 +12,9 @@ function highlightLinks() {
     if (matchedBookmark) {
       // Apply the styles to gray-out the link
       linkElement.classList.add(`highlighted-link-${index}`);
-      linkElement.style.setProperty("background-color", "rgba(24, 111, 101, 0.5)", "important");
-      linkElement.style.setProperty("filter", "grayscale(100%)", "important");
-      linkElement.style.setProperty("opacity", "0.75", "important");
+      // linkElement.style.setProperty("background-color", "rgba(0,0,0,0.5)", "important");
+      linkElement.style.setProperty("filter", "brightness(0) contrast(0)", "important");
+      linkElement.style.setProperty("opacity", "0", "important");
     }
   });
 }
