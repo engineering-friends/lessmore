f2a5951f7b942944feea6eb412a56b2b1e793884
marklidenberg <marklidenberg@gmail.com>
2024-10-21 22:32:09 +0600

    rollback

--- source/other/chrome-bookmarks-highlighter/content.js
+++ source/other/chrome-bookmarks-highlighter/content.js
@@ -10,18 +10,14 @@ function highlightLinks() {
     const fullURL = new URL(linkElement.getAttribute('href'), window.location).toString();
     const matchedBookmark = bookmarkLinks.find(bookmark => fullURL.startsWith(bookmark));
     if (matchedBookmark) {
+      // Apply the styles to gray-out the link
       linkElement.classList.add(`highlighted-link-${index}`);
-      // linkElement.style.setProperty("border", "2px solid #186F65", "important");
-      // linkElement.style.setProperty("border-radius", "8px", "important");
-      // linkElement.style.setProperty("padding", "4px", "important");
-      // linkElement.style.setProperty("color", "#186F65", "important");
-      // linkElement.style.setProperty("background-color", "rgba(24, 111, 101, 0.5)", "important");
-      styleSheet.insertRule(`.highlighted-link-${index}::after { content: " ✅"; }`, 0);
+      linkElement.style.setProperty("background-color", "rgba(24, 111, 101, 0.5)", "important");
+      linkElement.style.setProperty("filter", "grayscale(100%)", "important");
+      linkElement.style.setProperty("opacity", "0.75", "important");
     }
   });
 }
-
-
 
 
   highlightLinks();
---
+++ source/other/chrome-bookmarks-highlighter/readme.md
@@ -0,0 +1,3 @@
+# About 
+
+highlights grey all links that are in bookmarks. Useful to filter what you've already seen.
