7c70b32e4fa2ead8707275c576df14bd9c3d2e58
marklidenberg <marklidenberg@gmail.com>
2024-10-21 21:43:07 +0600

    rollback

--- source/other/safari-bookmarks-highlighter/Shared (Extension)/Resources/background.js
+++ source/other/safari-bookmarks-highlighter/Shared (Extension)/Resources/background.js
@@ -1,35 +1,6 @@
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log("Received request: ", request);
 
-    if (request.greeting === "hello") {
-        sendResponse({ farewell: "goodbye" });
-        return true;
-    }
-
-    if (request.action === "getBookmarks") {
-        browser.bookmarks.getTree().then((bookmarkTreeNodes) => {
-            const bookmarks = flattenBookmarks(bookmarkTreeNodes);
-            const response = { bookmarks: bookmarks.slice(0, 5) };
-            console.log("Sending bookmarks:", response);
-            sendResponse(response);
-        }).catch((error) => {
-            console.error("Error fetching bookmarks:", error);
-            sendResponse({ error: error.toString() });
-        });
-        return true; // Indicates that we will send a response asynchronously
-    }
+    if (request.greeting === "hello")
+        return Promise.resolve({ farewell: "goodbye" });
 });
-
-function flattenBookmarks(bookmarkTreeNodes) {
-    let bookmarks = [];
-    for (const node of bookmarkTreeNodes) {
-        if (node.url) {
-            bookmarks.push({ title: node.title, url: node.url });
-        }
-        if (node.children) {
-            bookmarks = bookmarks.concat(flattenBookmarks(node.children));
-        }
-    }
-    console.log("Flattened bookmarks:", bookmarks);
-    return bookmarks;
-}
