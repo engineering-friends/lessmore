69a662bf27b1c46219e62bf1628a9bf25f6646bf
marklidenberg <marklidenberg@gmail.com>
2024-10-21 21:15:01 +0600

    update

--- source/other/safari-bookmarks-highlighter/Shared (Extension)/Resources/content.js
+++ source/other/safari-bookmarks-highlighter/Shared (Extension)/Resources/content.js
@@ -1,3 +1,4 @@
+// Existing message handling code
 browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
     console.log("Received response: ", response);
 });
@@ -5,3 +6,19 @@ browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log("Received request: ", request);
 });
+
+// New code to highlight elements
+function highlightElements() {
+    const elements = document.querySelectorAll('a[href="https://www.iana.org/domains/example"]');
+    elements.forEach(element => {
+        element.style.backgroundColor = 'yellow';
+        element.style.border = '2px solid red';
+    });
+}
+
+// Run the highlight function when the page is loaded
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', highlightElements);
+} else {
+    highlightElements();
+}
