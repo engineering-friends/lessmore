d73fa9997885342939977bf74a8a99206223ee9e
marklidenberg <marklidenberg@gmail.com>
2024-10-22 15:08:05 +0600

    rollback

--- source/other/chrome-bookmarks-highlighter/content.js
+++ source/other/chrome-bookmarks-highlighter/content.js
@@ -10,11 +10,37 @@ function highlightLinks() {
     const fullURL = new URL(linkElement.getAttribute('href'), window.location).toString();
     const matchedBookmark = bookmarkLinks.find(bookmark => fullURL.startsWith(bookmark));
     if (matchedBookmark) {
-      // Apply the styles to gray-out the link
+      // - Make element disappear
       linkElement.classList.add(`highlighted-link-${index}`);
-      // linkElement.style.setProperty("background-color", "rgba(0,0,0,0.5)", "important");
-      linkElement.style.setProperty("filter", "brightness(0) contrast(0)", "important");
       linkElement.style.setProperty("opacity", "0", "important");
+
+      // - Make parent element with thumbnail disappear
+
+    // Assuming linkElement is the current <a> element
+      let parentElement = linkElement.parentElement;
+
+      while (parentElement) {
+        let hasThumbnailInAttribute = false;
+
+        // Loop through all attributes of the current element
+        for (let attr of parentElement.attributes) {
+          if ((attr.name.toLowerCase().includes('thumbnail'))  || (attr.value.toLowerCase().includes('thumbnail'))) {
+            hasThumbnailInAttribute = true;
+            break;
+          }
+        }
+
+        if (hasThumbnailInAttribute) {
+          break; // Found the element with "thumbnail" in the attribute name
+        }
+
+        parentElement = parentElement.parentElement; // Move to the next parent
+      }
+
+      if (parentElement) {
+        parentElement.style.setProperty("opacity", "0", "important");
+      }
+
     }
   });
 }
